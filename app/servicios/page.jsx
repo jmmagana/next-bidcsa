c6289891de62b7b298a25eca79ead809f3e38c58
jmmagana <josemanuel.magana@gmail.com>
2024-03-29 00:42:16 +0100

    Create ´servicios´ page

---
+++ app/servicios/page.jsx
@@ -0,0 +1,8 @@
+export default function ServiciosPage() {
+  return (
+    <>
+      <h1>Nuestros Servicios</h1>
+      <p>Aquí presentamos nuestros servicios</p>
+    </>
+  );
+}
---
+++ app/servicios/proyectos/page.jsx
@@ -0,0 +1,10 @@
+export default function ProyectosPage() {
+  return (
+    <>
+      <h1>Gerencia de Proyectos de Construcción</h1>
+      <p>
+        Planeación, dirección y ejecución de las diferentes etapas del proyecto
+      </p>
+    </>
+  );
+}
