68f6ad7ff92c54d606308ad2f6cbb13dce0a2c1a
jmmagana <josemanuel.magana@gmail.com>
2024-03-30 21:12:49 +0100

    Add hyperlinks

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,25 @@
 import { Children } from "react";
+import Link from "next/link";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <body>
-        <header>[header]</header>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/experiencia">Experiencia</Link>
+              </li>
+              <li>
+                <Link href="/servicios">Servicios</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
         <main>{children}</main>
         <footer>[footer]</footer>
       </body>
